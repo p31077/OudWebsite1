f7ca0a84fc50f1ba7c5539ed6eb2d9adc584e8d7
Punnapat Tissa <a0819135547@gmail.com>
2025-12-07 20:56:55 +0700

    v1.1
    test

---
+++ App1.py
@@ -0,0 +1,7 @@
+import os
+import streamlit as st
+from tomlkit import key
+
+st.title("Welcome หมูตุ๋น!!!")
+st.header("คิดถึงเค้ารึเปล่าเอ่ย *ไอ่อ้วน*")
+st.image(os.path.join(os.getcwd(), "static", "166962.jpg"), width=300)
